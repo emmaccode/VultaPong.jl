644f0e856d2f563ed90dee248ba947f0ee2d8c2b
emmy <52672675+emmettgb@users.noreply.github.com>
2023-01-04 20:48:21 -0600

    large updates; collision

--- src/VultaPong.jl
+++ src/VultaPong.jl
@@ -11,28 +11,35 @@ The home function is served as a route inside of your server by default. To
     change this, view the start method below.
 """
 function home(c::Connection)
+    player_force::Int64 = 40
     pongbod = body("mainbody")
-    on(c, pongbod, "click") do cm::ComponentModifier
-        cm["paddle-left"] = "x" => 400
+    pongpaddle_left = rect("paddleleft", width = 40,
+    height = 160)
+    pongball = circle("mycirc", "stroke-width" => 5px, "r" => 20)
+    km = ToolipsSession.KeyMap()
+    bind!(km, "w") do cm::ComponentModifier
+        force!(cm, pongpaddle_left, Vec2(0, -10))
     end
-    Vulta.open(c, pongbod) do vm::VultaModifier
-        pongpaddle_left = rect("paddle-left", width = 10,
-        height = 50)
-        pongball = circle("mycirc", "stroke-width" => 5px, "r" => 10)
-        if vm.delta == 1
-            spawn!(vm, pongball, Vec2(640, 360))
-
-            spawn!(vm, pongpaddle_left, Vec2(620, 360))
-        elseif vm.delta == 55
-            force!(vm, pongpaddle_left, Vec2(10, 0))
-        else
-            if is_colliding(vm, pongpaddle_left, pongball)
-                style!(vm.cm, pongpaddle_left, "color" => "red")
-            else
-    #            style!(vm, pongpaddle_left, "color" => "black")
+    on(c, pongbod, "click") do cm::ComponentModifier
+        force!(cm, pongball, Vec2(-20, 0))
+    end
+    bind!(km, "s") do cm::ComponentModifier
+        force!(cm, pongpaddle_left, Vec2(0, 10))
+    end
+    Vulta.initialize(c, pongbod) do cm::ComponentModifier
+        if initializing(c)
+            bind!(c, cm, km, on = :press)
+            spawn!(cm, pongball, Vec2(640, 360))
+            spawn!(cm, pongpaddle_left, Vec2(50, 500))
+        end
+        if pongpaddle_left.name in keys(cm.rootc)
+            if is_colliding(cm, pongpaddle_left, pongball)
+                cm[pongpaddle_left] = "fx" => "0"
+                cm[pongpaddle_left] = "fy" => "0"
+                cm[pongball] = "fx" => player_force
+                cm[pongball] = "fy" => cm[pongpaddle_left]["fy"]
             end
         end
-
     end
 end
 
@@ -41,7 +48,7 @@ fourofour = route("404") do c
 end
 
 routes = [route("/", home), fourofour]
-extensions = Vector{ServerExtension}([Logger(), Files(), Session(), ])
+extensions = Vector{ServerExtension}([Logger(), Files(), Session(), VultaCore()])
 """
 start(IP::String, PORT::Integer, ) -> ::ToolipsServer
 --------------------
